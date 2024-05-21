b2f162f69085563db0072a5d78076945013705d4
ScottBlyth <37386802+ScottBlyth@users.noreply.github.com>
2024-05-21 23:12:36 +1000

    simple SIRD stochastic model implemented

--- country.py
+++ country.py
@@ -6,6 +6,7 @@ Created on Fri May 17 11:47:01 2024
 """
 import numpy as np
 from matplotlib import pyplot as plt
+from disease import disease
 
 # utils 
 
@@ -18,19 +19,22 @@ def fmap_vec(functions):
     return wrapped_function
 
 # from workshop/lectures
-def gillespie(events, event_consequences, Y0, t0, t_max=100):
+def gillespie(Y0, t0, t_max=100, max_iter=10**4):
     y, t = Y0, t0
-    Y, T = np.array(Y0), [t]
-    while t<t_max:
+    Y, T = np.array(Y0.get_info()), [t]
+    i = 0
+    while t<t_max and i <= max_iter:
+        events,event_consequences = y.get_SIRD_events()
         p = propensities(t, y, events=events)
         p_rel = p/sum(p)
         tte = [time_to_event(p[i]) for i in range(len(p))]
         idx = np.random.choice(range(len(p)), p=p_rel)
         event, dt = event_consequences[idx], tte[idx]
-        y = event(t, y)
+        event(t)
         t += dt    
         T += [t]
-        Y = np.vstack([Y, y])
+        Y = np.vstack([Y, y.get_info()])
+        i += 1
     return T, Y
 
 time_to_event = lambda p: (-1/p)*np.log(np.random.random())
@@ -38,32 +42,52 @@ time_to_event = lambda p: (-1/p)*np.log(np.random.random())
 def propensities(t, y, events):
     e_ = []
     for event in events:
-        e = event(t=t, y=y)
+        e = event(t=t)
         e_.append(e)
     return e_
 
 class Country:
-    def __init__(self, birth_rate, y_0, disease): 
+    def __init__(self, birth_rate, y_0, infection : disease): 
+        # self.current = [S,I,R,D]
         self.current = y_0 
-        self.disease = disease 
+        self.disease = infection 
         self.birth_rate = birth_rate
         self.neighbours = []
+        
+    def copy(self):
+        c = Country(self.birth_rate, self.current, self.disease)
+        # in the future, copy neighbours to
+        return c 
+    
+    def get_info(self): 
+        return np.copy(self.current)
         
     def add_neighbour(self, l_i_j, l_j_i, country):
         self.neighbours.append((l_i_j, l_j_i, country))
         
+    def move_i_to_j(self, i, j):
+        if self.current[i] == 0:
+            return
+        self.current[i] -= 1 # S
+        self.current[j] += 1 # I 
+        
     def get_SIRD_events(self): 
         S,I,R,D = self.current
         l1,l2,l3,l4 = self.disease.get_params()
-        S_to_I = lambda t, y : l1*S*I 
-        I_to_R = lambda t, y : l2*I
-        R_to_S = lambda t, y : l4*R 
-        I_to_D = lambda t, y : l3*I  
+        S_to_I = lambda t : l1*S*I 
+        I_to_R = lambda t : l2*I
+        R_to_S = lambda t : l4*R 
+        I_to_D = lambda t : l3*I  
+        props = np.array([S_to_I, I_to_R, R_to_S, I_to_D])
+        # consequences
+        event_S_I = lambda t : self.move_i_to_j(0, 1)
+        event_I_R = lambda t : self.move_i_to_j(1, 2)
+        event_R_S = lambda t : self.move_i_to_j(2, 0)
+        event_I_D = lambda t : self.move_i_to_j(1, 3)
         
-        props = lambda t, y : fmap_vec([S_to_I(t,y), I_to_R(t,y), R_to_S(t,y), I_to_D(t,y)])
+        events = np.array([event_S_I, event_I_R, event_R_S, event_I_D])
         
-        return np.array([S_to_I, I_to_R])
-        
+        return props,events
         
     def events(self): 
         S,I,R,D = self.current
@@ -80,7 +104,14 @@ class Country:
     def event_consequences(self): 
         S,I,R,D = self.current
         
-        
-        
+    
+if __name__ == "__main__":
+    d = disease(0.00005, 1, 0.002, 0.00001)
+    c = Country(1, np.array([10**5, 100, 0, 0]), d)
+    T,Y = gillespie(c, 0,  t_max=100, max_iter=10**7)
+    plt.plot(T,Y)
+    plt.legend(["S", "I", "R", "D"])
+    
+    
     
 
---
+++ disease.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 15:40:32 2024
+
+@author: scott
+"""
+
+
+class disease: 
+    def __init__(self, l1,l2,l3,l4):
+        # l1 : infection rate
+        # l2 : recovery rate
+        # l3 : immunity 
+        # l4 : mortality rate
+        self.l1,self.l2,self.l3,self.l4 = l1,l2,l3,l4 
+        
+    def get_params(self):
+        return self.l1,self.l2,self.l3,self.l4
+
+    def copy(self):
+        return disease(self.l1,self.l2,self.l3,self.l4)
