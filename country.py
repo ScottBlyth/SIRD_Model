53abd24dcd17079884b59c2399757c79a9874807
ScottBlyth <37386802+ScottBlyth@users.noreply.github.com>
2024-05-22 17:21:31 +1000

    country to country travel added
    the propensties and event consequences added to the SIRD model

--- country.py
+++ country.py
@@ -7,6 +7,7 @@ Created on Fri May 17 11:47:01 2024
 import numpy as np
 from matplotlib import pyplot as plt
 from disease import disease
+from bisect import bisect_left 
 
 # utils 
 
@@ -21,10 +22,11 @@ def fmap_vec(functions):
 # from workshop/lectures
 def gillespie(Y0, t0, t_max=100, max_iter=10**4):
     y, t = Y0, t0
-    Y, T = np.array(Y0.get_info()), [t]
+    #Y, T = np.array(Y0.get_info()), [t]
+    T = [t]
     i = 0
     while t<t_max and i <= max_iter:
-        events,event_consequences = y.get_SIRD_events()
+        events,event_consequences = y.get_events()
         p = propensities(t, y, events=events)
         p_rel = p/sum(p)
         tte = [time_to_event(p[i]) for i in range(len(p))]
@@ -33,9 +35,9 @@ def gillespie(Y0, t0, t_max=100, max_iter=10**4):
         event(t)
         t += dt    
         T += [t]
-        Y = np.vstack([Y, y.get_info()])
+        #Y = np.vstack([Y, y.get_info()])
         i += 1
-    return T, Y
+    return None
 
 time_to_event = lambda p: (-1/p)*np.log(np.random.random())
 
@@ -47,12 +49,15 @@ def propensities(t, y, events):
     return e_
 
 class Country:
-    def __init__(self, birth_rate, y_0, infection : disease): 
+    mu = 10
+    def __init__(self, id: int, birth_rate, y_0, infection : disease): 
         # self.current = [S,I,R,D]
         self.current = y_0 
         self.disease = infection 
         self.birth_rate = birth_rate
         self.neighbours = []
+        self.id = id
+        self.history = np.array([y_0])
         
     def copy(self):
         c = Country(self.birth_rate, self.current, self.disease)
@@ -71,6 +76,13 @@ class Country:
         self.current[i] -= 1 # S
         self.current[j] += 1 # I 
         
+    def move_to_country(self, SIRD_index, neighbour): 
+        # locate neighbour 
+        l1,l2,country = self.neighbours[neighbour]
+        country.current[SIRD_index] += 1
+        self.current[SIRD_index] -= 1
+        
+    
     def get_SIRD_events(self): 
         S,I,R,D = self.current
         l1,l2,l3,l4 = self.disease.get_params()
@@ -88,29 +100,58 @@ class Country:
         events = np.array([event_S_I, event_I_R, event_R_S, event_I_D])
         
         return props,events
+    
+    def country_to_country(self): 
+        # propensties
+        lst = []
+        for l1,l2,c in self.neighbours: 
+            lst.append(lambda t : l1*self.current[0]) # S 
+            lst.append(lambda t : l1*self.current[1]/Country.mu) # I 
+            lst.append(lambda t : l1*self.current[2]) # R 
+        props = np.array(lst)
+        events = []
+        for i,val in enumerate(self.neighbours):
+            l1,l2,c = val
+            events.append(lambda t : self.move_to_country(0, i))
+            events.append(lambda t : self.move_to_country(1, i))
+            events.append(lambda t : self.move_to_country(2, i))
+        events = np.array(events)
+        return props,events
         
-    def events(self): 
-        S,I,R,D = self.current
-        l1,l2,l3,l4 = self.disease.get_params()
-        beta = self.birth_rate 
-        
-        dSdt = lambda t,y : -l1*S*I + l4 * R + beta 
-        dIdt = lambda t,y : l1*S*I - l2*I - l3*I 
-        dRdt = lambda t,y : l2*R - l4 * R 
-        dDdt = lambda t,y : l3*I
-        dYdt = lambda t,y : np.array([dSdt(t,y), dIdt(t, y), dRdt(t,y), dDdt(t,y)])
-        return dYdt
+    def get_events(self): 
+        p1,e1 = self.get_SIRD_events()
+        p2,e2 = self.country_to_country()
+        p = np.concatenate([p1,p2])
+        e = np.concatenate([e1,e2])
+        return p,e
     
     def event_consequences(self): 
         S,I,R,D = self.current
         
     
+class World:
+    def __init__(self, countries):
+        self.countries  = countries
+        
+    def get_events(self):
+        props = []
+        events = []
+        for c in self.countries: 
+            p,e = c.get_events() 
+            props += list(p) 
+            events += list(e)
+        props = np.array(props)
+        events = np.array(events)
+        return props,events
+    
 if __name__ == "__main__":
-    d = disease(0.00005, 1, 0.002, 0.00001)
-    c = Country(1, np.array([10**5, 100, 0, 0]), d)
-    T,Y = gillespie(c, 0,  t_max=100, max_iter=10**7)
-    plt.plot(T,Y)
-    plt.legend(["S", "I", "R", "D"])
+    d = disease(0.000005, 0.1, 0.0002, 0.0001)
+    c = Country(0,1, np.array([10**5, 100, 0, 0]), d)
+    c2 = Country(0,1, np.array([10**5, 0, 0, 0]), d)
+    c.add_neighbour(0.001, 0.001, c2)
+    c2.add_neighbour(0.001, 0.001, c)
+    gillespie(World([c,c2]), 0,  t_max=100, max_iter=10**6)
+    w = World([c,c2])
     
     
     
