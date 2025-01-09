0d3546bc814e934f24ada2f0abdd5fa83d75114a
ScottBlyth <37386802+ScottBlyth@users.noreply.github.com>
2025-01-09 12:34:57 +1100

    added zone to add cities (nodes) in

--- SIR/src/main/java/com/example/sir/SIRController.java
+++ SIR/src/main/java/com/example/sir/SIRController.java
@@ -1,8 +1,13 @@
 package com.example.sir;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 public class SIRController {
 
@@ -10,11 +15,21 @@ public class SIRController {
     private LineChart<Number, Number> epiChart;
 
     @FXML
+    private Pane cityGraph;
+
+    @FXML
     public void initialize() {
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
         series.getData().add(new XYChart.Data<>(1,2));
         series.getData().add(new XYChart.Data<>(2,3));
+        series.setName("Infections");
         epiChart.getData().add(series);
+    }
+
+    @FXML
+    public void clickOnGraph(MouseEvent mouseEvent) {
+        Circle circle = new Circle(mouseEvent.getX(), mouseEvent.getY(), 10, Paint.valueOf("blue"));
+        cityGraph.getChildren().add(circle);
 
     }
 
